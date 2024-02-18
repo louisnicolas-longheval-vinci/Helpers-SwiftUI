8f547c0a8e29cca70ac6d0a1007d4e8b0b18f225
surex <samuser107@gmail.com>
2024-02-18 15:18:08 +0100

    Initial Commit

---
+++ ExpandingGradient/ExpandingGradientApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandingGradientApp.swift
+//  ExpandingGradient
+//
+//  Created by Louni Longheval on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpandingGradientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
